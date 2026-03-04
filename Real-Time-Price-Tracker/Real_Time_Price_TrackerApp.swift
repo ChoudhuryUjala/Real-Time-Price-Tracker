62bdd6113421f4487226b79b1e22eed2e0a04360
Ujala <42828898+ChoudhuryUjala@users.noreply.github.com>
2026-03-05 00:17:03 +0400

    adding project files and workspace
    committing project files and test files.

---
+++ Real-Time-Price-Tracker/Real_Time_Price_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Real_Time_Price_TrackerApp.swift
+//  Real-Time-Price-Tracker
+//
+//  Created by Ujala on 05/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Real_Time_Price_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Real-Time-Price-TrackerTests/Real_Time_Price_TrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  Real_Time_Price_TrackerTests.swift
+//  Real-Time-Price-TrackerTests
+//
+//  Created by Ujala on 05/03/26.
+//
+
+import Testing
+
+struct Real_Time_Price_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
